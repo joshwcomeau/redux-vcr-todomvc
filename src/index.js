89cc90864b77b1eb899609d12a9a5973cb76cb2f
Josh Comeau <josh@breather.com>
2016-09-16 07:55:27 -0400

    Initial cassette configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
 
-const store = createStore(reducer)
+import { createCaptureMiddleware, createPersistHandler, createRetrieveHandler, createRetrieveMiddleware, createReplayMiddleware, wrapReducer, getQueryParam } from 'redux-vcr';
+
+const firebaseAuth = {
+  apiKey: "AIzaSyBklunZBwf0YMFbpHWVmfcu2scFAb2gq3o",
+  authDomain: "todomvc-ccb8e.firebaseapp.com",
+  databaseURL: "https://todomvc-ccb8e.firebaseio.com",
+};
+
+const persistHandler = createPersistHandler({ firebaseAuth });
+const retrieveHandler = createRetrieveHandler({ firebaseAuth });
+
+const middlewares = [
+  createCaptureMiddleware({ persistHandler }),
+  createRetrieveMiddleware({
+    retrieveHandler,
+    initialCassetteId: getQueryParam({ param: 'cassetteId' }),
+  }),
+  createReplayMiddleware({ maximumDelay: 1000 }),
+];
+
+const store = createStore(
+  wrapReducer(reducer),
+  applyMiddleware(...middlewares)
+);
 
 render(
   <Provider store={store}>
